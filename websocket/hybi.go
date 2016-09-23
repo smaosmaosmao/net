b486b9d59569eadb09cd1a4998dd3ac197a4c874
duser <duser@duser-dekstop.com>
2016-09-23 12:09:20 +0300

    .

--- websocket/hybi.go
+++ websocket/hybi.go
@@ -442,8 +442,8 @@ func hybiClientHandshake(config *Config, br *bufio.Reader, bw *bufio.Writer) (er
 		return err
 	}
 	if resp.StatusCode != 101 || resp.StatusCode != 201  {
-		fmt.Println("Huston we got an eror")
-		return ErrBadFrame
+		fmt.Println("Huston we got an eror: %s", resp.StatusCode)
+		return ErrBadStatus
 	}
 	if strings.ToLower(resp.Header.Get("Upgrade")) != "websocket" ||
 		strings.ToLower(resp.Header.Get("Connection")) != "upgrade" {
